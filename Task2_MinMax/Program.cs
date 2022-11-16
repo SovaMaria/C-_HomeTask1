4afb6e0c74addf483efc7ace5e4152836eea2f29
M_Sova <MyMail@mail.ru>
2022-11-16 19:11:56 +0300

    initial commit2

--- Task2_MinMax/Program.cs
+++ Task2_MinMax/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("Макс = ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("Макс = ");
+    Console.WriteLine(number2);
+}
